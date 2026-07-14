bc9c0e4a380be4443513bb1902470bd75fc52eff
Akrishna <pananthakrishnan939@gmail.com>
2026-07-14 15:15:26 +0530

    Vessel Planning works - Mobile and Tab

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,8 +4,8 @@ class AppConfig {
 
   // static const String baseUrl = "http://192.168.1.100:8085"; // Local
   // static const String baseUrl = "http://103.215.139.121:9001"; // Demo
-static const String baseUrl = "http://103.215.139.8:8001"; // Demo Latest
-//static const String baseUrl = "https://maleva.my"; // Live
+//static const String baseUrl = "http://103.215.139.8:8001"; // Demo Latest
+static const String baseUrl = "https://maleva.my"; // Live
 
 }
 
